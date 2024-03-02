0ab12ddcaa4c357368425152a49cd1aab1f78aab
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-02 19:26:47 -0300

    fix: add ArrayList<Curso> inside Curso class

--- src/main/java/com/fmt/catalog/model/Curso.java
+++ src/main/java/com/fmt/catalog/model/Curso.java
@@ -2,6 +2,8 @@ package com.fmt.catalog.model;
 
 import lombok.Getter;
 
+import java.util.ArrayList;
+
 public class Curso {
 
     private static Integer proximoId = 1;
@@ -18,11 +20,17 @@ public class Curso {
     @Getter
     private int cargaHoraria;
 
+    public static ArrayList<Curso> cursosList = new ArrayList<>();
+
     public Curso(String nomeDoCurso, String descricao, int cargaHoraria) {
         this.id = proximoId++;
         this.nomeDoCurso = nomeDoCurso;
         this.descricao = descricao;
         this.cargaHoraria = cargaHoraria;
+    }
+
+    public static void inserirCursoEmCursosList(Curso curso) {
+        cursosList.add(curso);
     }
 
 }
