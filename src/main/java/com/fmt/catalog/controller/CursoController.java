b26f7286ecddfac042b9980470b2a11210f5d03f
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-02 19:28:58 -0300

    feat: create GET and POST methods to endpoint /curso

--- src/main/java/com/fmt/catalog/controller/CursoController.java
+++ src/main/java/com/fmt/catalog/controller/CursoController.java
@@ -1,4 +1,29 @@
 package com.fmt.catalog.controller;
 
+import com.fmt.catalog.model.Curso;
+import com.fmt.catalog.service.CursoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/curso")
 public class CursoController {
+
+    private final CursoService cursoService;
+
+    public CursoController(CursoService cursoService) {
+        this.cursoService = cursoService;
+    }
+
+    @GetMapping
+    public ArrayList<Curso> getTodosCursos() {
+        return cursoService.consultarTodosCursos();
+    }
+
+    @PostMapping
+    public void postCurso(@RequestBody Curso curso) {
+        cursoService.cadastrarCurso(curso);
+    }
+
 }
