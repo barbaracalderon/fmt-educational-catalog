d8519fead033f51bf10cb23c779e9a8467c5f499
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-02 20:27:43 -0300

    feat: create methods encontrarCursoPorId and matricularAlunoEmCurso

--- src/main/java/com/fmt/catalog/service/CursoService.java
+++ src/main/java/com/fmt/catalog/service/CursoService.java
@@ -1,10 +1,16 @@
 package com.fmt.catalog.service;
 
+import com.fmt.catalog.model.Aluno;
 import com.fmt.catalog.model.Curso;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 
+@Service
 public class CursoService {
+
+    AlunoService alunoService;
+
 
     public ArrayList<Curso> consultarTodosCursos() {
         return Curso.cursosList;
@@ -12,6 +18,21 @@ public class CursoService {
 
     public void cadastrarCurso(Curso curso) {
         Curso.inserirCursoEmCursosList(curso);
+    }
+
+    public Curso encontrarCursoPorId(Integer id) {
+        return Curso.cursosList.get(id);
+    }
+
+    public void matricularAlunoEmCurso(Integer idCurso, Integer idAluno) {
+        Curso curso = encontrarCursoPorId(idCurso);
+        Aluno aluno = alunoService.encontrarAlunoPorId(idAluno);
+
+        if (curso != null && aluno != null) {
+            ArrayList<Aluno> alunosMatriculados = curso.getAlunosMatriculadosList();
+            alunosMatriculados.add(aluno);
+            curso.setAlunosMatriculados(alunosMatriculados);
+        }
     }
 
 }
