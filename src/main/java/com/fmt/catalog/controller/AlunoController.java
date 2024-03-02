28be211123fcd17526664516488210f9c6701ad9
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-02 18:47:28 -0300

    feat: create AlunoController class

--- src/main/java/com/fmt/catalog/controller/AlunoController.java
+++ src/main/java/com/fmt/catalog/controller/AlunoController.java
@@ -1,4 +1,26 @@
 package com.fmt.catalog.controller;
 
+import com.fmt.catalog.model.Aluno;
+import com.fmt.catalog.service.AlunoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/aluno")
 public class AlunoController {
+
+    private final AlunoService alunoService;
+
+    public AlunoController(AlunoService alunoService) {
+        this.alunoService = alunoService;
+    }
+
+    @GetMapping
+    public ArrayList<Aluno> getTodosAlunos() {
+        return alunoService.getTodosAlunos();
+    }
+
 }
