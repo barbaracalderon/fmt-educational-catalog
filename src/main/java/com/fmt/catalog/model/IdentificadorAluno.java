b51b4eaf683157aa2c1fcb907f6212225de1c49b
barbaracalderon <bcalderoni.ti@gmail.com>
2024-03-04 02:36:15 -0300

    feat: create new class to be sent as BodyRequest

--- src/main/java/com/fmt/catalog/model/IdentificadorAluno.java
+++ src/main/java/com/fmt/catalog/model/IdentificadorAluno.java
@@ -1,4 +1,16 @@
 package com.fmt.catalog.model;
 
+import lombok.Data;
+
+@Data
 public class IdentificadorAluno {
+    private Integer idAluno;
+
+    public Integer getIdAluno() {
+        return idAluno;
+    }
+
+    public void setIdAluno(Integer idAluno) {
+        this.idAluno = idAluno;
+    }
 }
